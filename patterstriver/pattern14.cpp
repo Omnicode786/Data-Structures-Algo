13e844d0cd591557e8371b360281ecb1cef3891d
Omnicode786 <muzammilalam408@gmail.com>
2025-03-15 03:55:45 +0500

    up

--- patterstriver/pattern14.cpp
+++ patterstriver/pattern14.cpp
@@ -7,11 +7,12 @@ void print(int n){
 
 for (int i = 0; i < n; i++)
 {
+    char ch = 'E';
+    ch -= i;
     for (int j = 0; j <= i; j++)
     {
-        char ch = 'E';
-        ch -=j; 
         cout<<ch;
+        ch++; 
     }
     cout <<endl;
     
