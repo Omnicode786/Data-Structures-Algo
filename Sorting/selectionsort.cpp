815a265b8f271a0344e1b01e8b6c84308a7e0b01
Omnicode786 <muzammilalam408@gmail.com>
2025-03-22 20:58:20 +0500

    sort

--- Sorting/selectionsort.cpp
+++ Sorting/selectionsort.cpp
@@ -1,3 +1,33 @@
 //  selection sort my method lets do dry run
 
 
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    int array[5] = {2,3,1,5,4};
+    int min = array[0];
+    for (int i = 0; i < 5; i++)
+    {
+        if (min > array[i])
+        {
+            min = array[i];
+        }
+        swap(array[i],min);
+        
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout <<array[i]<< " ";
+    }
+    
+    
+
+
+
+}
