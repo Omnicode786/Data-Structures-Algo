51e3ef37fe6f2cc3831d707289fe7cc9d2791428
Omnicode786 <muzammilalam408@gmail.com>
2025-04-10 17:55:56 +0500

    up

--- LEETCODE1/Leaders in an array/brute.cpp
+++ LEETCODE1/Leaders in an array/brute.cpp
@@ -4,9 +4,42 @@ using namespace std;
 
 vector <int> leader(int a[], int n){
 vector <int> result;
+bool leaderornot = true;
+for (int i = 0; i < n; i++)
+{
+    leaderornot = true;
+    for (int j = i+1; j < n; j++)
+    {
+        if (a[i] < a[j])
+        {
+            leaderornot = false;
+            break;
+        }
+        
 
+    }
+    if (leaderornot)
+    {
+        result.push_back(a[i]);
+    }
+    
+}
+return result;
 
 
 
 }
 
+
+
+int main(){
+int a[6] = {10,22,4,5,3,2};
+vector <int> resulter = leader(a,6);
+for (auto i:resulter){
+
+cout <<i<<" ";
+
+}
+
+
+}
---
+++ LEETCODE1/Leaders in an array/optimal.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> Leader(int a[],int n){
+
+    
+
+
+
+}
