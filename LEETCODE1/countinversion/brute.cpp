4bb66063dff0e76f765d0861d8ee6edfcce260e4
Muzammil Alam <164224694+Omnicode786@users.noreply.github.com>
2025-07-21 12:26:15 -0700

    comit

--- LEETCODE1/countinversion/brute.cpp
+++ LEETCODE1/countinversion/brute.cpp
@@ -1,2 +1,24 @@
 #include <bits/stdc++.h>
-using namespace std
+using namespace std;
+
+int countinversion(vector<int> nums, int n){
+int count = 0;
+    for (int i = 0; i < n - 1; i++){
+        for (int j = i + 1; j < n; j++){
+            if (nums[i] > nums[j]){
+                count++;
+            }
+        }
+    }
+return count;
+
+}
+
+int main(){
+
+vector <int> nums = {5,3,2,4,1};
+
+int number = countinversion(nums,5);
+cout << number;
+
+}
---
+++ LEETCODE1/countinversion/optimal.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int countinversion(vector <int> nums, int n){
+    for (int i = 0; i < n-1; i++){
+        int j = i + 1;
+        
+    }
+}
