20960f87fa7da8c031856a97af07cc0070f2a527
Muzammil Alam <muzammilalam408@gmail.com>
2025-07-22 00:33:58 +0500

    up

--- LEETCODE1/countinversion/example.cpp
+++ LEETCODE1/countinversion/example.cpp
@@ -1,3 +1,18 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
+int countinversion(vector<int> nums,vector<int> nums2, int m, int n){
+    int left = 0;
+    int right = 0;
+    int count = 0;
+    while (left < m && right < n)
+    {
+        if (nums[left] )
+        {
+            /* code */
+        }
+        
+    }
+    
+}
